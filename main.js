abffe886df378890ff717209ff03a29a69404aea
lilo <skellycantsleep@gmail.com>
2026-03-28 17:42:16 +0400

    just experimenting

---
+++ main.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client();
+const dt = require('downloadtiktok');
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready`);
+});
+
+const prefix = '.'
+
+client.on('messageCreate', (msg, args) => {
+    if(msg.content.startsWith(`${prefix}test`))
+        msg.channel.send('test works')
+})
+
+
+client.login(process.env.Token);
